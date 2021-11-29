6cd83344698912c5c6fc4313904fc13122d6ddad
zwendo <lorris.creantor@gmail.com>
2021-11-29 20:17:20 +0100

    Knot8MethodVisitor refactor.

--- knot8-compiler-plugin/src/main/kotlin/com/zwendo/knot8/plugin/Knot8MethodVisitor.kt
+++ knot8-compiler-plugin/src/main/kotlin/com/zwendo/knot8/plugin/Knot8MethodVisitor.kt
@@ -1,21 +1,41 @@
 package com.zwendo.knot8.plugin
 
-import com.zwendo.knot8.plugin.adapter.annotation.NOT_ZERO
+import com.zwendo.knot8.plugin.adapter.NotZeroAnnotationVisitor.Companion.NOT_ZERO
+import com.zwendo.knot8.plugin.adapter.NotZeroAnnotationVisitor
 import org.jetbrains.org.objectweb.asm.AnnotationVisitor
 import org.jetbrains.org.objectweb.asm.MethodVisitor
 
-internal class Knot8MethodVisitor(original: MethodVisitor) : MethodVisitor(API_VERSION, original) {
+internal class Knot8MethodVisitor(
+    private val original: MethodVisitor,
+    private val methodType: Knot8ClassBuilder.MethodType
+) :
+    MethodVisitor(API_VERSION, original) {
 
-    override fun visitAnnotation(descriptor: String, visible: Boolean): AnnotationVisitor {
-        return findAnnotationVisitor(descriptor) { super.visitAnnotation(descriptor, visible) }
+    val onVisitCode = mutableListOf<() -> Unit>()
+
+    override fun visitParameterAnnotation(parameter: Int, descriptor: String, visible: Boolean): AnnotationVisitor {
+        visitSpecificAnnotation(descriptor, AnnotationTarget.PARAMETER, original, visible)
+        return super.visitParameterAnnotation(parameter, descriptor, visible)
     }
 
+    override fun visitCode() {
+        onVisitCode.forEach { it() }
+        super.visitCode()
+    }
+
+    override fun visitEnd() {
+        // TODO visit stacked instructions
+        super.visitEnd()
+    }
+
+    private fun visitSpecificAnnotation(
+        descriptor: String,
+        target: AnnotationTarget,
+        visitor: MethodVisitor,
+        visible: Boolean
+    ): Unit {
+        when (descriptor) {
+            NOT_ZERO -> NotZeroAnnotationVisitor(target, visitor, this);
+        }
+    }
 }
-
-
-private fun findAnnotationVisitor(descriptor: String, default: () -> AnnotationVisitor): AnnotationVisitor =
-    when (descriptor) {
-        NOT_ZERO -> default()
-        else -> default()
-    }
-
