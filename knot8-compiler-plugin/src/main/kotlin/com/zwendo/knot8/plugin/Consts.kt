aa6d42f7d1479fcac0d852b36e310c3f30083fd7
zwendo <lorris.creantor@gmail.com>
2021-11-29 20:10:27 +0100

    Added consts.

--- knot8-compiler-plugin/src/main/kotlin/com/zwendo/knot8/plugin/Consts.kt
+++ knot8-compiler-plugin/src/main/kotlin/com/zwendo/knot8/plugin/Consts.kt
@@ -2,7 +2,14 @@ package com.zwendo.knot8.plugin
 
 import org.jetbrains.org.objectweb.asm.Opcodes
 
+// Project ASM api version
+internal const val API_VERSION: Int = Opcodes.ASM9
 
-internal const val API_VERSION = Opcodes.ASM9
+// Project group id in bytecode version
+internal const val BYTECODE_GROUP: String = "com/zwendo/"
 
+// Project root project in bytecode
+internal const val BYTECODE_PROJECT: String = "knot8/"
 
+// Project annotations path in bytecode
+internal const val BYTECODE_ANNOTATIONS_PATH: String = BYTECODE_GROUP + BYTECODE_PROJECT + "annotation/"
