ebc53a460c9f958f8fe4378e58a648ba7f4b829e
zwendo <lorris.creantor@gmail.com>
2021-12-01 21:02:08 +0100

    Added method to gather parameters name

--- knot8-compiler-plugin/src/main/kotlin/com/zwendo/knot8/plugin/Knot8ClassBuilder.kt
+++ knot8-compiler-plugin/src/main/kotlin/com/zwendo/knot8/plugin/Knot8ClassBuilder.kt
@@ -5,17 +5,12 @@ import org.jetbrains.kotlin.codegen.DelegatingClassBuilder
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOrigin
 import org.jetbrains.org.objectweb.asm.MethodVisitor
+import org.jetbrains.org.objectweb.asm.Type
 
-internal class Knot8ClassBuilder internal constructor(
+internal class Knot8ClassBuilder(
     private val delegateBuilder: ClassBuilder,
     val configuration: CompilerConfiguration
-) :
-    DelegatingClassBuilder() {
-
-    internal enum class MethodType {
-        METHOD,
-        CONSTRUCTOR
-    }
+) : DelegatingClassBuilder() {
 
     override fun getDelegate(): ClassBuilder = delegateBuilder
 
@@ -28,11 +23,20 @@ internal class Knot8ClassBuilder internal constructor(
         exceptions: Array<out String>?
     ): MethodVisitor {
         val original = super.newMethod(origin, access, name, desc, signature, exceptions)
-        val type: MethodType = when (name) {
-            "<init>" -> MethodType.CONSTRUCTOR
-            else -> MethodType.METHOD
-        }
-        return Knot8MethodVisitor(original, type)
+        val parameters = parameters(origin.descriptor.toString(), desc)
+        return Knot8MethodVisitor(original, access, name, desc, signature ?: "", exceptions ?: arrayOf(), parameters)
     }
 
+    private fun parameters(paramNameDesc: String, paramTypeDesc: String): List<FunctionParameter> {
+        val types = Type.getArgumentTypes(paramTypeDesc)
+        val names = paramNameDesc.slice(IntRange(paramNameDesc.indexOf('(') + 1, paramNameDesc.lastIndexOf(')')))
+            .split(", ")
+            .map { it.replaceAfter(':', "").dropLast(1) }
+
+        val parameters = mutableListOf<FunctionParameter>()
+        for (i in types.indices) {
+            parameters.add(FunctionParameter(names[i], types[i]))
+        }
+        return parameters
+    }
 }
