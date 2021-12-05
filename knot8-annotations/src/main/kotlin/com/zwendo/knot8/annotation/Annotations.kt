e5de041f5d6664cfaa61080181607c929a465124
zwendo <lorris.creantor@gmail.com>
2021-12-05 01:06:44 +0100

    Added NotEmpty and BasicContainer annotations.

--- knot8-annotations/src/main/kotlin/com/zwendo/knot8/annotation/Annotations.kt
+++ knot8-annotations/src/main/kotlin/com/zwendo/knot8/annotation/Annotations.kt
@@ -2,10 +2,20 @@ package com.zwendo.knot8.annotation
 
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION, AnnotationTarget.FIELD)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.FIELD)
 annotation class NotZero
 
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION, AnnotationTarget.FIELD)
+@Target( AnnotationTarget.FUNCTION, AnnotationTarget.FIELD)
 annotation class PosOrZero
+
+@MustBeDocumented
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
+annotation class NotEmpty
+
+@MustBeDocumented
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.FIELD)
+annotation class BasicContainer(val suffix: String = "")
